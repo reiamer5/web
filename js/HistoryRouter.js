63926888d9b1a88a1029f54fbb75fa82b3c71570
reiam <841887695@qq.com>
2022-12-15 15:58:26 +0800

    修改

---
+++ js/HistoryRouter.js
@@ -0,0 +1,43 @@
+class HistoryRouter {
+    constructor(routes) {
+      this.el = document.querySelector('#router-view')
+      this.init(routes)
+    }
+  
+    init(routes) {
+      console.log('HistoryRouter init')
+      const onChange = this.change.bind(this)
+      
+      const old = window.onload
+      window.onload = function () {
+        onChange()
+        const linkList = document.querySelectorAll('a[href]')
+        linkList.forEach((link) => {
+          link.addEventListener('click', (e) => {
+            e.preventDefault()
+            history.pushState(null, '', link.getAttribute('href'))
+            onChange()
+          })
+        })
+        window.onload = old
+      }
+      
+      const mapper = {}
+      for (const route of routes) {
+        mapper[route.path] = route
+      }
+      this.mapper = mapper
+      
+      window.addEventListener('popstate', onChange)
+    }
+  
+    change() {
+      const path = location.pathname
+      console.log(`hash path: ${path}`)
+      this.el.innerHTML = this.mapper[path].content
+    }
+  }
+  
+  const router = new HistoryRouter(routes)
+  console.log(router)
+  
---
+++ js/routes.js
@@ -0,0 +1,10 @@
+const routes = [
+  {
+    path: './',
+    content: 'Default content',
+  },
+  {
+    path: './list.html',
+    content: 'This is list page',
+  },
+]
